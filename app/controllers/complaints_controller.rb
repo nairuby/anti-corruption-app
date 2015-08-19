e6be77c4f416a31c35bda81259bcd0a2e8d568db
mboya <mboyaberry@gmail.com>
2015-08-19 11:17:45 +0300

    mods

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,23 +1,15 @@
 class ComplaintsController < ApplicationController
-
   #layout "complaint"
-
   def index
     @complaints = Complaint.newest_first
   end
-
-
   def show
     @complaint = Complaint.find(params[:id])
   end
-
-
   def new
     @complaint = Complaint.new 
-    flash[:notice] = "Complaint submitted successfully"
+#    flash[:notice] = "Complaint submitted successfully"
   end
-
-
   def create
     # Instantiate a new object using form parameters
     @complaint = Complaint.new(complaint_params)
@@ -28,42 +20,35 @@ class ComplaintsController < ApplicationController
     else
       # If save fails, redisplay the form so user can fix problems
       render('new')
+    end
   end
-end 
-
   def edit
     @complaint = Complaint.find(params[:id])
   end
-
-
   def update
     # Find an existing object using form parameters
     @complaint = Complaint.find(params[:id])
     # Update the object
     if @complaint.update_attributes(complaint_params)
       # If update succeeds, redirect to the index action
-      flash[:notice] = "Complaint Updated successfully"
+#      flash[:notice] = "Complaint Updated successfully"
       redirect_to(:action => 'show', :id => @complaint.id)
     else
       # If update fails, redisplay the form so user can fix problems
       render('edit')
     end
   end
-
-
   def delete
     @complaint = Complaint.find(params[:id])
   end
-
   def destroy
-  complaint = Complaint.find(params[:id]).destroy
-  flash[:notice] = "Complaint '#{complaint.name}' destroyed successfully"
-  redirect_to(:action => 'index')
- end 
-
+      complaint = Complaint.find(params[:id]).destroy
+      flash[:notice] = "Complaint '#{complaint.name}' destroyed successfully"
+      redirect_to(:action => 'index')
+  end
 end
-  private
 
+  private
     def complaint_params
       # same as using "params[:subject]", except that it:
       # - raises an error if :subject is not present
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def index
+      @complaints = Complaint.newest_first
   end
+    def show
+        @complaint = Complaint.find(params[:id])
+    end
     def hotzones
     end
     def forum
